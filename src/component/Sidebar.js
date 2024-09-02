e13c3b28f3bdd121cc55b51e30d5fbcc57506358
Sabarish <sabarish.user@gmail.com>
2024-09-02 18:06:03 +0530

    first commit

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { FaUserAlt, FaMoneyCheckAlt, FaCogs, FaChartBar, FaClock } from 'react-icons/fa';
-import img from '../../src/images/college logo.jpg';
+import { FaHome, FaBox, FaLayerGroup, FaHistory, FaMusic,FaShoppingBag , FaSteam,FaPlay } from 'react-icons/fa';
+
 
 const Sidebar = () => {
   return (
-    <div style={{ backgroundColor: 'blue', color: '#ecf0f1', width: '200px', height: '120vh', padding: '20px' }}>
+    <div style={{ backgroundColor: 'white', color: '#ecf0f1', width: '40px', height: '120vh', padding: '20px' ,boxShadow:"2px 2px 2px 2px gray"}}>
       <div style={{ marginBottom: '30px', textAlign: 'center' }}>
-        <img src={img} alt="Logo" />
       </div>
       <div>
-        <p style={{fontSize:"30px"}}><FaUserAlt style={{fontSize:"20px"}} /><br/> Student Management</p>
-        <p style={{fontSize:"20px"}}><FaMoneyCheckAlt style={{fontSize:"20px"}} /><br/> Financial Management</p>
-        <p style={{fontSize:"20px"}}><FaCogs style={{fontSize:"20px"}} /><br/> Quality Control</p>
-        <p style={{fontSize:"20px"}}><FaChartBar style={{fontSize:"20px"}} /><br/> Report Delivery</p>
-        <p style={{fontSize:"20px"}}><FaClock style={{fontSize:"20px"}} /><br/> Attendance</p>
+        <p style={{fontSize:"30px"}}><FaHome style={{fontSize:"20px",color:"black"}} /></p>
+        <p style={{fontSize:"20px"}}><FaBox style={{fontSize:"20px",color:"black"}}  /> </p>
+        <p style={{fontSize:"20px"}}><FaLayerGroup style={{fontSize:"20px",color:"black"}}  /></p>
+        <p style={{fontSize:"20px"}}><FaHistory style={{fontSize:"20px",color:"black"}} /></p>
+        <p style={{fontSize:"20px"}}><FaMusic style={{fontSize:"20px",color:"black"}}  /></p>
+        <p style={{fontSize:"20px"}}><FaShoppingBag style={{fontSize:"20px",color:"black"}}  /></p>
+        <p style={{fontSize:"20px"}}><FaSteam style={{fontSize:"20px",color:"black"}}  /><br/></p>
+        <p style={{fontSize:"20px"}}><FaPlay style={{fontSize:"20px",color:"black"}}  /><br/></p>
       </div>
     </div>
   );
